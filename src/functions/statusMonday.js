fd1dce9906f514b1ff507578543e0d95e1afe514
MukaDeveloper <muka.bertoni@gmail.com>
2024-01-31 17:39:20 -0300

    fix

--- src/functions/statusMonday.js
+++ src/functions/statusMonday.js
@@ -1,40 +1,38 @@
 module.exports = (client) => {
-    client.statusMonday = async () => {
-        const express = require('express');
-        const bodyParser = require('body-parser');
-        require('dotenv').config();
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    require('dotenv').config();
 
-        const app = express();
-        const port = process.env.port || 3000;
+    const app = express();
+    const port = process.env.port || 3000;
 
-        app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-        app.get("/", (req, res) => {
-            res.send("Hello World!");
+    app.get("/", (req, res) => {
+        res.send("Hello World!");
+    });
+
+    app.post("/status", async (req, res) => {
+        const data = req.body;
+        res.send({
+            status: "success",
+            challenge: data.challenge,
         });
 
-        app.post("/status", async (req, res) => {
-            const data = req.body;
-            res.send({
-                status: "success",
-                challenge: data.challenge,
-            });
+        const number = process.env.wppnumberStatus || message.from
+        await client.sendMessage(number, `*Atualização de Status em RW PJ:*\n\nNome do elemento: ${data.event.pulseName}\nStatus anterior: _${data.event.previousValue.label.text}_\nStatus atualizado: _${data.event.value.label.text}_`);
+    });
 
-            const number = process.env.wppnumberStatus || message.from
-            await client.sendMessage(number, `*Atualização de Status em RW PJ:*\n\nNome do elemento: ${data.event.pulseName}\nStatus anterior: _${data.event.previousValue.label.text}_\nStatus atualizado: _${data.event.value.label.text}_`);
+    app.post("/update", async (req, res) => {
+        const data = req.body;
+        res.send({
+            status: "success",
+            challenge: data.challenge,
         });
+        console.log(`\n\n\nAdição de Comentário em RW PJ:\n Comentário: ${data?.event?.textBody}`);
+    });
 
-        app.post("/update", async (req, res) => {
-            const data = req.body;
-            res.send({
-                status: "success",
-                challenge: data.challenge,
-            });
-            console.log(`\n\n\nAdição de Comentário em RW PJ:\n Comentário: ${data?.event?.textBody}`);
-        });
-
-        app.listen(port, () => {
-            console.log("Webhook carregado na porta " + port);
-        });
-    }
+    app.listen(port, () => {
+        console.log("Webhook carregado na porta " + port);
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,48 +1,53 @@
 const path = require('path');
 const fs = require('fs')
 
-const rootPath = path.join(__dirname, '..');
-const sessionPath = path.join(rootPath, 'session.json');
+require('dotenv').config();
 
-let sessionData;
-if (fs.existsSync(sessionPath)) {
-    sessionData = require(sessionPath);
-}
+const { Client, RemoteAuth } = require('whatsapp-web.js');
+// CONEXÃO COM MONGOOSE
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: rootPath,
-    })
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
+
+    client.initialize();
 });
 
-
+// CARREGANDO FUNÇÕES
 const functionsDir = path.join(__dirname, 'functions');
 const functionFiles = fs.readdirSync(functionsDir);
-
 function importFunctions(client) {
     functionFiles.forEach((file) => {
         const functionsPath = path.join(functionsDir, file);
         require(functionsPath)(client);
     })
 }
-
 importFunctions(client);
-client.statusMonday();
 
+
+// EXPORTANDO QR CODE PARA REPLIT
 const axios = require('axios');
 client.on('qr', (qr) => {
     console.log("Postando QR Code para servidor na Repl.it")
-    axios.post("https://f38ab128-cf3b-4ba8-b748-39d02b289463-00-2eayhqmxctp36.janeway.replit.dev/qrcode", {
+    axios.post(process.env.replitPost, {
         qrcode: qr
     })
     // qrcode.generate(qr, { small: true });
 });
 
+
+
+// EVENTO READY
 client.on('ready', () => {
     console.log('Aplicação conectada com sucesso! API funcionando!');
 });
-
-client.initialize();
 
 module.exports = client;

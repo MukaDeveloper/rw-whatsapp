cdc188a9fb08f6fb6148418d7a35cb44deeff143
MukaDeveloper <muka.bertoni@gmail.com>
2024-02-08 15:53:28 -0300

    Change puppeteer client configuration for no sandbox

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-    // puppeteer: {
-    // 	args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    // },
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
     authStrategy: new LocalAuth({
         dataPath: path.join(process.cwd())
     })
